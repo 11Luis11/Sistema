592e8b35343383bb7a9f58b0808b1c227676bda3
Luis Tarazona <ltarazonam@autonoma.edu.pe>
2025-11-17 21:43:27 -0500

    Login Corregido con mensaje de 3 intentos

--- auto-push.bat
+++ auto-push.bat
@@ -1,10 +1,6 @@
 @echo off
-echo === AUTO PUSH ACTIVADO ===
-echo Cancelar con CTRL + C
-
-:loop
+set /p msg=Escribe el mensaje del commit: 
 git add .
-git commit -m "Auto-update"
+git commit -m "%msg%"
 git push
-timeout /t 5 >nul
-goto loop
+pause
